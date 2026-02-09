106b422cfce0f858d73173ebf50871fbeea68ece
NiveskZ <krsmjogos@outlook.com>
2026-02-09 15:21:12 -0300

    feat: MODIFY

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import sqlalchemy
 
+from feature_engine import encoding, selection, imputation
+from sklearn import model_selection
+
 pd.set_option('display.max_rows',None)
 # %%
 conn = sqlalchemy.create_engine("sqlite:///data/analytics/database.db")
@@ -30,7 +33,6 @@ df_train_test = df[df['dtRef'] < df['dtRef'].max()].reset_index(drop=True)
 X = df_train_test[features]
 y = df_train_test[target]
 # %%
-from sklearn import model_selection
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
     X, y,
@@ -42,7 +44,6 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(
 print(f"Base Treino: {y_train.shape[0]} Unid. | Tx. Target {100*y_train.mean():.2f}%")
 print(f"Base Treino: {y_test.shape[0]} Unid. | Tx. Target {100*y_test.mean():.2f}%")
 # %%
-
 # EXPLORE -- MISSING
 
 s_nas = X_train.isna().mean()
@@ -62,18 +63,49 @@ df_train[num_features] = df_train[num_features].astype(float)
 
 bivariada = df_train.groupby(target)[num_features].median().T
 bivariada['ratio'] = (bivariada[1] + 0.001) / (bivariada[0] + 0.001)
+bivariada.sort_values(by='ratio', ascending=False)
 
 to_remove = bivariada[bivariada['ratio'] == 1].index.tolist()
+# %%
+df_train.groupby('descLifeCycleAtual')[target].mean()
 
-for i in to_remove:
-    features.remove(i)
-    num_features.remove(i)
-#%%
-bivariada.sort_values(by='ratio', ascending=False)
+# %%
+df_train.groupby('descLifeCycleD28')[target].mean()
+
 # %%
 
-bivariada_cat = df_train.groupby('descLifeCycleAtual')[target].mean()
-bivariada_cat
+# MODIFY -- DROP
+X_train[num_features] = X_train[num_features].astype(float)
+
+drop_features = selection.DropFeatures(to_remove)
+
 # %%
-bivariada_cat = df_train.groupby('descLifeCycleD28')[target].mean()
-bivariada_cat
+# MODIFY -- FILL NA
+
+fill_0 = ['github2025','python2025']
+
+imput_0 = imputation.ArbitraryNumberImputer(arbitrary_number=0, variables=fill_0)
+
+imput_new = imputation.CategoricalImputer(fill_value='Nao-Usuario',
+                                          variables=['descLifeCycleD28'])
+
+imput_1000 = imputation.ArbitraryNumberImputer(
+    arbitrary_number=1000,
+    variables=['avgIntervaloDiasVida','avgIntervaloDiasD28','qtdDiasUltAtividade']
+    )
+
+# %%
+# MODIFY -- ONEHOT
+
+onehot = encoding.OneHotEncoder(variables=cat_features)
+
+# MODIFY -- Aplicando as transformações
+X_train_transform = drop_features.fit_transform(X_train)
+X_train_transform = imput_0.fit_transform(X_train_transform)
+X_train_transform = imput_new.fit_transform(X_train_transform)
+X_train_transform = imput_1000.fit_transform(X_train_transform)
+X_train_transform = onehot.fit_transform(X_train_transform)
+
+# %%
+X_train_transform.head()
+# %%
